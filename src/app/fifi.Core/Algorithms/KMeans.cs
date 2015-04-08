32cedbcb2b28291cae37ff59a8a7aa64a690bc5f
Omar Ali Sheikh <sheikhomar@gmail.com>
2015-04-08 18:43:48 +0200

    Initial classes.

---
+++ src/app/fifi.Core/Algorithms/KMeans.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fifi.Core.Algorithms
+{
+  /// <summary>
+  /// Represents k-means clustering algorithm.
+  /// </summary>
+  public class KMeans : IClusteringAlgorithm
+  {
+  }
+}
