01b5338f93aecd5fb1cb54e3637790255f4b5908
Marc Beuchert <marcbeuchert@hotmail.com>
2015-04-09 12:26:58 +0200

    Tilføjet elementer til Profile og lavet CSV import

--- src/app/fifi.Core/Profile.cs
+++ src/app/fifi.Core/Profile.cs
@@ -8,5 +8,21 @@ namespace fifi.Core
 {
     public class Profile
     {
+        public string CreatedAt { get; set; }
+        public string Gender { get; set; }
+        public string EmploymentStatus { get; set; }
+        public string[] ReadingHabits { get; set; }
+        public string[] BookGenres { get; set; }
+        public string[] FilmGenres { get; set; }
+        public string RelationshipStatus { get; set; }
+        public string HouseholdSize { get; set; }
+        public string[] Gadgets { get; set; }
+        public string Children { get; set; }
+        public string HousingType { get; set; }
+        public string HouseholdControl { get; set; }
+        public string Salary { get; set; }
+        public string MajorPurchase { get; set; }
+        public string PurchaseDecision { get; set; }
+        public int BirthYear { get; set; }
     }
 }
