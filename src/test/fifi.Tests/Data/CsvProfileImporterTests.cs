4a570a9f93f7543abf8ed3013738de9e3e2d1eb0
Omar Ali Sheikh <sheikhomar@gmail.com>
2015-04-12 15:44:57 +0200

    Add test fixture for CsvProfileImporter.

---
+++ src/test/fifi.Tests/Data/CsvProfileImporterTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using fifi.Core;
+using fifi.Data;
+using System.IO;
+
+namespace fifi.Tests.Data
+{
+    [TestFixture]
+    public class CsvProfileImporterTests
+    {
+        private IList<Profile> results;
+
+        [SetUp]
+        public void Setup()
+        {
+            var reader = new StringReader(Resources.SampleData);
+            var importer = new CsvProfileImporter(reader);
+            results = importer.Run();
+        }
+
+        [Test]
+        public void ShouldImportAllLines()
+        {
+            Assert.AreEqual(results.Count, 2);
+        }
+
+        [Test]
+        public void ShouldParseGenderCorrectly()
+        {
+            Assert.AreEqual(results[0]["Gender"].Value, 1);
+            Assert.AreEqual(results[1]["Gender"].Value, 0);
+        }
+    }
+}
