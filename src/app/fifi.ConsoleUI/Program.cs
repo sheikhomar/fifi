f2d8d86b07bccf097dc21841fb49651c46245541
Omar Ali Sheikh <sheikhomar@gmail.com>
2015-04-12 15:37:34 +0200

    Simple method to manually test CSV import.

--- src/app/fifi.ConsoleUI/Program.cs
+++ src/app/fifi.ConsoleUI/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using fifi.Data;
 
 namespace fifi.ConsoleUI
 {
@@ -13,6 +15,13 @@ namespace fifi.ConsoleUI
             // TODO: Fancy stuff
             Console.WriteLine("fifi");
             Console.ReadKey();
+        }
+
+        static void TestImport()
+        {
+            var reader = new StreamReader("UserData.csv");
+            var importer = new CsvProfileImporter(reader);
+            importer.Run();
         }
     }
 }
