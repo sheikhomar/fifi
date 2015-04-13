b4484a417641d7b517b12800d1f062f416b61585
Omar Ali Sheikh-Omar <omar@keepfocus.dk>
2015-04-13 11:01:48 +0200

    Implement k-means clustering. WIP.

--- src/app/fifi.Core/Algorithms/KMeans.cs
+++ src/app/fifi.Core/Algorithms/KMeans.cs
@@ -13,21 +13,45 @@ namespace fifi.Core.Algorithms
   {
       private int k;
       private DataSet dataSet;
+      private int maxIterations;
+      private IDistanceMetric distanceMetric;
 
-      public KMeans(DataSet dataSet, int k)
+      public KMeans(DataSet dataSet, int k, IDistanceMetric distanceMetric, int maxIterations = 100)
       {
           this.dataSet = dataSet;
           this.k = k;
+          this.maxIterations = maxIterations;
+          this.distanceMetric = distanceMetric;
       }
 
       public IList<Cluster> Generate()
       {
           IList<Cluster> clusters = new List<Cluster>();
+          IList<Centroid> centroids = GenerateRandomCentroids();
 
           int dimensions = dataSet.CountDimensions;
 
+          for (int i = 0; i < maxIterations; i++)
+          {
+              do
+              {
+                  foreach (var profile in dataSet)
+                  {
+                      foreach (var centroid in centroids)
+                      {
+                          distanceMetric.Calculate(centroid.Values, profile.Values);
+                      }
+                  }
+              } while (CentroidsMove());
+          }
 
           return clusters;
+      }
+
+      private bool CentroidsMove()
+      {
+          // TODO: Implement centroids movement detection.
+          return true;
       }
 
       private IList<Centroid> GenerateRandomCentroids()
