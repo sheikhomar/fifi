de0fceb5e729ea6883d1f7f3455c32e670ed33f6
Omar Ali Sheikh-Omar <omar@keepfocus.dk>
2015-04-13 10:45:23 +0200

    Add class that represents a data set.

---
+++ src/app/fifi.Core/DataSet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fifi.Core
+{
+    public class DataSet
+    {
+        private IList<Profile> profiles;
+        private int dimensions = 0;
+
+        public DataSet()
+        {
+            profiles = new List<Profile>();
+        }
+
+        public void AddItem(Profile profile)
+        {
+            if (dimensions == 0)
+                dimensions = profile.CountValues;
+            profiles.Add(profile);
+        }
+
+        public int CountDimensions
+        {
+            get { return dimensions; }
+        }
+    }
+}
