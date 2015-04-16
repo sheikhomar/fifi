024d88a7205f3f732ec3e9a8b46ccf310f9b44bd
MBSource <mathiasbsvendsen@gmail.com>
2015-04-16 10:50:58 +0200

    Fixed K-Means loops

--- src/app/fifi.Core/Algorithms/KMeans.cs
+++ src/app/fifi.Core/Algorithms/KMeans.cs
@@ -6,128 +6,136 @@ using System.Threading.Tasks;
 
 namespace fifi.Core.Algorithms
 {
-  /// <summary>
-  /// Represents k-means clustering algorithm.
-  /// </summary>
-  public class KMeans : IClusteringAlgorithm
-  {
-      private int k;
-      private DataSet dataSet;
-      private int maxIterations;
-      private IDistanceMetric distanceMetric;
+    /// <summary>
+    /// Represents k-means clustering algorithm.
+    /// </summary>
+    public class KMeans : IClusteringAlgorithm
+    {
+        private int k;
+        private DataSet dataSet;
+        private int maxIterations;
+        private IDistanceMetric distanceMetric;
 
-      public KMeans(DataSet dataSet, int k, IDistanceMetric distanceMetric, int maxIterations = 100)
-      {
-          this.dataSet = dataSet;
-          this.k = k;
-          this.maxIterations = maxIterations;
-          this.distanceMetric = distanceMetric;
-      }
+        public KMeans(DataSet dataSet, int k, IDistanceMetric distanceMetric, int maxIterations = 100)
+        {
+            this.dataSet = dataSet;
+            this.k = k;
+            this.maxIterations = maxIterations;
+            this.distanceMetric = distanceMetric;
+        }
 
-      public ClusteringResult Generate()
-      {
-          ClusteringResult result = new ClusteringResult();
+        public ClusteringResult Generate()
+        {
+            ClusteringResult result = new ClusteringResult();
 
-          IList<Centroid> centroids = GenerateRandomCentroids();
-          foreach (var centroid in centroids)
-          {
-              result.Clusters.Add(new Cluster(centroid));
-          }
+            IList<Centroid> centroids = GenerateRandomCentroids();
+            foreach (var centroid in centroids)
+            {
+                result.Clusters.Add(new Cluster(centroid));
+            }
 
-          for (int i = 0; i < maxIterations; i++)
-          {
-              bool centroidMoved = false;
+            bool centroidMoved = false;
+            int count = 0;
 
-              do
-              {
-                  foreach (var profile in dataSet)
-                  {
-                      Centroid closestCentroid = null;
-                      Cluster closestCluster = null;
-                      double minDistance = double.MaxValue;
-                      foreach (var cluster in result.Clusters)
-                      {
-                          var centroid = cluster.Centroid;
-                          double distance = distanceMetric.Calculate(centroid.Values, profile.Values);
-                          if (distance < minDistance)
-                          {
-                              minDistance = distance;
-                              closestCentroid = centroid;
-                              closestCluster = cluster;
-                          }
-                      }
+            do
+            {
+                count++;
 
-                      ClusterMember member = new ClusterMember(profile, minDistance);
-                      closestCluster.Members.Add(member);
+                foreach (var cluster in result.Clusters)
+                {
+                    cluster.Members.Clear();
+                }
 
-                      closestCentroid.Add(profile);
-                  }
 
-                  foreach (var cluster in result.Clusters)
-                  {
-                      if (MoveCentroidIfNeeded(cluster))
-                      {
-                          centroidMoved = true;
-                      }
-                  }
+                foreach (var profile in dataSet)
+                {
+                    Centroid closestCentroid = null;
+                    Cluster closestCluster = null;
+                    double minDistance = double.MaxValue;
+                    foreach (var cluster in result.Clusters)
+                    {
+                        var centroid = cluster.Centroid;
+                        double distance = distanceMetric.Calculate(centroid.Values, profile.Values);
+                        if (distance < minDistance)
+                        {
+                            minDistance = distance;
+                            closestCentroid = centroid;
+                            closestCluster = cluster;
+                        }
+                    }
 
-              } while (centroidMoved);
-          }
+                    ClusterMember member = new ClusterMember(profile, minDistance);
+                    closestCluster.Members.Add(member);
 
-          return result;
-      }
+                    closestCentroid.Add(profile);
+                }
+                centroidMoved = false;
 
-      private bool MoveCentroidIfNeeded(Cluster cluster)
-      {
-          Centroid centroid = cluster.Centroid;
+                foreach (var cluster in result.Clusters)
+                {
+                    if (MoveCentroidIfNeeded(cluster))
+                        centroidMoved = true;
+                }
 
-          foreach (var member in cluster.Members)
-          {
-              var profile = member.Profile;
-              for (int i = 0; i < profile.CountValues; i++)
-              {
-                  centroid.GravityCenter[i] += profile.Values[i];
-              }
+                if (count >= maxIterations)
+                    centroidMoved = false;
 
-              for (int i = 0; i < centroid.GravityCenter.Length; i++)
-              {
-                  centroid.GravityCenter[i] /= cluster.Members.Count;
-              }
-          }
+            } while (centroidMoved);
 
-          bool centroidHasMoved = false;
+            return result;
+        }
 
-          for (int i = 0; i < centroid.Values.Length; i++)
-          {
-              if (centroid.Values[i] != centroid.GravityCenter[i])
-              {
-                  centroidHasMoved = true;
-              }
-          }
+        private bool MoveCentroidIfNeeded(Cluster cluster)
+        {
+            Centroid centroid = cluster.Centroid;
 
-          if (centroidHasMoved)
-          {
-              for (int i = 0; i < centroid.Values.Length; i++)
-              {
-                  centroid.Values[i] = centroid.GravityCenter[i];
-              }
-          }
+            foreach (var member in cluster.Members)
+            {
+                var profile = member.Profile;
+                for (int i = 0; i < profile.CountValues; i++)
+                {
+                    centroid.GravityCenter[i] += profile.Values[i];
+                }
 
-          return centroidHasMoved;
-      }
+                for (int i = 0; i < centroid.GravityCenter.Length; i++)
+                {
+                    centroid.GravityCenter[i] /= cluster.Members.Count;
+                }
+            }
 
-      private IList<Centroid> GenerateRandomCentroids()
-      {
-          IList<Centroid> centroids = new List<Centroid>();
+            bool centroidHasMoved = false;
 
-          int dimensions = dataSet.CountDimensions;
+            for (int i = 0; i < centroid.Values.Length; i++)
+            {
+                if (centroid.Values[i] != centroid.GravityCenter[i])
+                {
+                    centroidHasMoved = true;
+                }
+            }
 
-          for (int i = 0; i < k; i++)
-          {
-              centroids.Add(Centroid.GenerateRandom(dimensions));
-          }
+            if (centroidHasMoved)
+            {
+                for (int i = 0; i < centroid.Values.Length; i++)
+                {
+                    centroid.Values[i] = centroid.GravityCenter[i];
+                }
+            }
 
-          return centroids;
-      }
-  }
+            return centroidHasMoved;
+        }
+
+        private IList<Centroid> GenerateRandomCentroids()
+        {
+            IList<Centroid> centroids = new List<Centroid>();
+
+            int dimensions = dataSet.CountDimensions;
+
+            for (int i = 0; i < k; i++)
+            {
+                centroids.Add(Centroid.GenerateRandom(dimensions));
+            }
+
+            return centroids;
+        }
+    }
 }
