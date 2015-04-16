a81ae0915aa36921733639bc12c6430d60b7f5dc
FkirkK <Frederik_KKristensen@hotmail.com>
2015-04-16 14:55:35 +0200

    MDSCluster class added for MDS-crew
    Hyg jer

---
+++ src/app/fifi.Core/MDSCluster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fifi.Core
+{
+    class MDSCluster
+    {
+        public MDSCluster()
+        {
+            List<DataPoint> MDSDataPoints = new List<DataPoint>();
+        }
+
+        // MDS holdet skal indlæse alle data for et cluster i ovenstående liste.
+        // Det de til sidst skal returnere er en liste af MDSClusters -- altså denne klasse.
+        // I kan bruge constructoren eller lave en smart metode til at proppe jeres data ind i listen.
+        // For hvert datapoint i listen skal i tilgå member'et .SetValueXY()
+
+    }
+}
