7d1031e45422f8f8b2a08db9ee37f93a7236efc0
Omar Ali Sheikh-Omar <omar@keepfocus.dk>
2015-04-16 15:37:26 +0200

    Program run times.

--- src/app/fifi.ConsoleUI/Program.cs
+++ src/app/fifi.ConsoleUI/Program.cs
@@ -16,10 +16,14 @@ namespace fifi.ConsoleUI
     {
         static void Main(string[] args)
         {
+            var start = DateTime.Now;
+
             RunKMeans();
 
+            var diff = DateTime.Now - start;
+
             // TODO: Fancy stuff
-            Console.WriteLine("FiFi has finished...");
+            Console.WriteLine("FiFi has finished in {0} ms...", diff.TotalMilliseconds);
             Console.WriteLine("Press any key to kill me :)");
             Console.ReadKey();
 
@@ -30,18 +34,25 @@ namespace fifi.ConsoleUI
             var reader = new StreamReader("UserData.csv");
             var importer = new CsvProfileImporter(reader);
 
+
             var dataSet = importer.Run();
+            
+
+
             var k = 5;
             var distanceMetric = new EuclideanMetric();
 
             var kmeans = new KMeans(dataSet, k, distanceMetric);
+
+            var start = DateTime.Now;
             var result = kmeans.Generate();
+
             var fileName = string.Format("kmeans-output-{0:yyyy-MM-dd_HH_mm_ss}.txt", DateTime.UtcNow);
 
 
             FileStream fs = new FileStream(fileName, FileMode.CreateNew);
             StreamWriter writer = new StreamWriter(fs);
-            string codeName = "fisk";
+            string codeName = "fisk, completed in " + (DateTime.Now - start).TotalMilliseconds + " ms";
 
             writer.WriteLine("Test {1}: {0:yyyy-MM-dd HH:mm}", DateTime.Now, codeName);
             writer.WriteLine("Seed: {0}", Centroid.RandomSeed);
