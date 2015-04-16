008e2ac117aad2edd4bf90805bcf4bb9dce4d7c5
Omar Ali Sheikh-Omar <omar@keepfocus.dk>
2015-04-16 10:00:33 +0200

    Test k-means.

--- src/app/fifi.ConsoleUI/Program.cs
+++ src/app/fifi.ConsoleUI/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Configuration;
 using fifi.Data;
 using fifi.Data.Configuration.Import;
+using fifi.Core.Algorithms;
 
 
 namespace fifi.ConsoleUI
@@ -15,9 +16,49 @@ namespace fifi.ConsoleUI
     {
         static void Main(string[] args)
         {
+            RunKMeans();
+
             // TODO: Fancy stuff
             Console.WriteLine("fifi");
             Console.ReadKey();
+
+        }
+
+        static void RunKMeans()
+        {
+            var reader = new StreamReader("UserData.csv");
+            var importer = new CsvProfileImporter(reader);
+
+            var dataSet = importer.Run();
+            var k = 1;
+            var distanceMetric = new EuclideanMetric();
+
+            var kmeans = new KMeans(dataSet, k, distanceMetric);
+            var result = kmeans.Generate();
+
+            FileStream fs = new FileStream("kmeans-output.txt", FileMode.CreateNew);
+            StreamWriter writer = new StreamWriter(fs);
+
+            writer.WriteLine("Test: {0:yyyy-MM-dd HH:mm}", DateTime.Now);
+            writer.WriteLine("Seed: {0}", Centroid.RandomSeed);
+            writer.WriteLine();
+
+            for (int i = 0; i < result.Clusters.Count; i++)
+            {
+                var cluster = result.Clusters[i];
+                var sumOfId = cluster.Members.Sum(e => e.Profile.Id);
+                var firstId = cluster.Members[0].Profile.Id;
+                var lastId = cluster.Members.Last().Profile.Id;
+                
+                writer.WriteLine("Cluster {0}: total {1}, {2}, {3}", i+1, sumOfId, firstId, lastId);
+
+
+
+            }
+            
+            writer.Close();
+            fs.Close();
+
 
         }
 
