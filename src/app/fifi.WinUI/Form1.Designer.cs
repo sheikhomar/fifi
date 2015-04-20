b3fe4f34776b56c744f95ab1c3b9221b37d8a1c8
Omar Ali Sheikh-Omar <omar@keepfocus.dk>
2015-04-20 14:01:46 +0200

    GUI test.

--- src/app/fifi.WinUI/Form1.Designer.cs
+++ src/app/fifi.WinUI/Form1.Designer.cs
@@ -39,25 +39,28 @@
             // 
             chartArea1.Name = "ChartArea1";
             this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(0, -2);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(558, 347);
+            this.chart1.Size = new System.Drawing.Size(660, 470);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             this.chart1.Click += new System.EventHandler(this.chart1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(556, 344);
+            this.ClientSize = new System.Drawing.Size(660, 470);
             this.Controls.Add(this.chart1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
--- src/app/fifi.WinUI/Form1.cs
+++ src/app/fifi.WinUI/Form1.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Configuration;
+using fifi.Data;
+using fifi.Data.Configuration.Import;
+using fifi.Core.Algorithms;
 using fifi.Core;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
 
 namespace fifi.WinUI
 {
@@ -16,6 +21,7 @@ namespace fifi.WinUI
         public Form1()
         {
             InitializeComponent();
+            /*
             MDSCluster Cluster1 = new MDSCluster();
             Cluster1.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(2, 4));
             Cluster1.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(4, 4));
@@ -42,9 +48,53 @@ namespace fifi.WinUI
             input.Add(Cluster1);
             input.Add(Cluster2);
             input.Add(Cluster3);
+            
+
+
 
             ScatterPlot result = new ScatterPlot(input, chart1);
             result.Draw();
+            */
+            Test();
+        }
+
+        private void Test()
+        {
+            var reader = new StreamReader("UserData.csv");
+            var importer = new CsvProfileImporter(reader);
+            var dataSet = importer.Run();
+            var k = 5;
+            var distanceMetric = new EuclideanMetric();
+
+            var kmeans = new KMeans(dataSet, k, distanceMetric);
+            var result = kmeans.Generate();
+
+            DistanceMatrix distanceMatrix = new DistanceMatrix(result);
+            double[,] matrix = distanceMatrix.GenerateMatrix();
+
+            var mds = new MultiDimensionalScaling(matrix);
+            double[,] resultMatrix = mds.Calculate(); //a shitty name
+            MDSCluster Cluster1 = new MDSCluster();
+            
+
+            for (int row = 0; row < resultMatrix.GetLength(1); row++)
+            {
+                double x = resultMatrix[0, row];
+                double y = resultMatrix[1, row];
+                Cluster1.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(x, y));
+                
+            }
+            List<MDSCluster> input = new List<MDSCluster>();
+            input.Add(Cluster1);
+            var scatterPlot = new ScatterPlot(input, chart1);
+            scatterPlot.Draw();
+
+
+            ChartArea area = chart1.ChartAreas[0];
+
+            area.AxisX.Crossing = 0;
+            area.AxisY.Crossing = 0;
+            
 
 
         }
