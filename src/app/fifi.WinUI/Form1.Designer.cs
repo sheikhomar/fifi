566abb9ad57facb0fffd45f379ef73938ffcf8cf
FkirkK <Frederik_KKristensen@hotmail.com>
2015-04-20 10:31:47 +0200

    ScatterPlot is AOK (somewhat) - now needs 2 inputs
    Raise your Kappa!

--- src/app/fifi.WinUI/Form1.Designer.cs
+++ src/app/fifi.WinUI/Form1.Designer.cs
@@ -28,20 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, -2);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(558, 347);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(556, 344);
+            this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- src/app/fifi.WinUI/Form1.cs
+++ src/app/fifi.WinUI/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using fifi.Core;
 
 namespace fifi.WinUI
 {
@@ -15,6 +16,42 @@ namespace fifi.WinUI
         public Form1()
         {
             InitializeComponent();
+            MDSCluster Cluster1 = new MDSCluster();
+            Cluster1.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(2, 4));
+            Cluster1.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(4, 4));
+            Cluster1.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(3, 3.5));
+            Cluster1.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(1, 2));
+            Cluster1.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(2, 3.7));
+            Cluster1.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(2.3, 4.1));
+            Cluster1.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(2.5, 4.8));
+            Cluster1.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(2.8, 3));
+
+            MDSCluster Cluster2 = new MDSCluster();
+            Cluster2.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(1, 1));
+            Cluster2.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(1, 1.2));
+            Cluster2.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(1.1, 1.2));
+            Cluster2.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(1.4, 0.78));
+            Cluster2.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(0.8, 0.7));
+
+            MDSCluster Cluster3 = new MDSCluster();
+            Cluster3.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(8, 6));
+            Cluster3.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(8.6, 8));
+            Cluster3.MDSDataPoints.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(9, 7.4));
+
+            List<MDSCluster> input = new List<MDSCluster>();
+            input.Add(Cluster1);
+            input.Add(Cluster2);
+            input.Add(Cluster3);
+
+            ScatterPlot result = new ScatterPlot(input, chart1);
+            result.Draw();
+
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
