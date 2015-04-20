92138d0f1a59c2284ddbfc955128269aa0b7e4a7
Marc Beuchert <marcbeuchert@hotmail.com>
2015-04-20 08:38:46 +0200

    DOne MDS

--- src/app/fifi.Core/MDS.cs
+++ src/app/fifi.Core/MDS.cs
@@ -3,28 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.LinearAlgebra.Factorization;
+
 
 namespace fifi.Core
 {
-    class MDS
+    public class MDS
     {
         double[,] distanceMatrix;
         double[,] squaredMatrix;
         double[,] jMatrix;
         double[,] scalarProductMatrix;
-        
+        double[,] result;
         
         public MDS(double[,] data)
         {
             distanceMatrix = data;
         }
         
-        public void Run()
+        public double[,] Run()
         {
             squaredMatrix = squaredDistanceMatrix(distanceMatrix);
             jMatrix = jMatrixCalculator(squaredMatrix);
             scalarProductMatrix = scalarProductMatrixCalculator(squaredMatrix, jMatrix);
-
+            result = eigenCalculator(scalarProductMatrix);
+            return result;
         }
 
         private double[,] squaredDistanceMatrix(double[,] distanceMatrix)
@@ -41,7 +46,7 @@ namespace fifi.Core
 
         private double[,] jMatrixCalculator(double[,] squaredMatrix)
         {
-            double[,] identityMatrix = new double[squaredMatrix.GetLength(0), squaredMatrix.GetLength(1)];
+            double[,] identityMatrix = identityMatrixGenerator(squaredMatrix.GetLength(1));
             double[,] tempJMatrix = new double[squaredMatrix.GetLength(0), squaredMatrix.GetLength(1)];
             double[,] oneMatrix = new double[squaredMatrix.GetLength(0), squaredMatrix.GetLength(1)];
             double dimensionScaling = Math.Pow(squaredMatrix.GetLength(0), -1);
@@ -50,11 +55,6 @@ namespace fifi.Core
             {
                 for (int col = 0; col < squaredMatrix.GetLength(1); col++)
                 {
-                    if (row == col)
-                        identityMatrix[row, col] = 1; 
-                    else
-                        identityMatrix[row, col] = 0;
-
                     oneMatrix[row, col] = 1;
                 }
             }
@@ -78,12 +78,48 @@ namespace fifi.Core
             {
                 for (int col = 0; col < squaredMatrix.GetLength(1); col++)
                 {
-                    tempScalarProductMatrix[row, col] = -(1 / 2) * jMatrix[row, col];
+                    tempScalarProductMatrix[row, col] = -(0.5) * jMatrix[row, col];
                 }
             }
             tempArray = matrixMultiplier(tempScalarProductMatrix, squaredMatrix);
             tempScalarProductMatrix = matrixMultiplier(tempArray, jMatrix);
             return tempScalarProductMatrix;
+        }
+
+        private double[,] eigenCalculator(double[,] scalarProductMatrix)
+        {
+            var convertedMatrix = DenseMatrix.OfArray(scalarProductMatrix);
+            var eigen = convertedMatrix.Evd();
+
+            double[] valueArray = eigen.EigenValues.Select(x => x.Real).ToArray();
+            double[,] valueResult = { {Math.Sqrt(valueArray[0]), 0}, {0, Math.Sqrt(valueArray[1])} };
+
+            double[,] vectorArray = eigen.EigenVectors.ToArray();
+            double[,] vectorResult = new double[2, scalarProductMatrix.GetLength(0)];
+
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < vectorArray.GetLength(0); col++)
+                {
+                    vectorResult[row, col] = vectorArray[col, row];
+                }
+            }
+
+            var valueResultMatrix = DenseMatrix.OfArray(valueResult);
+            var vectorResultMatrix = DenseMatrix.OfArray(vectorResult);
+            var ResultMatrix = valueResultMatrix.Multiply(vectorResultMatrix);
+            double[,] returnResult = ResultMatrix.ToArray();
+            double[,] final = new double[returnResult.GetLength(0), returnResult.GetLength(1)];
+
+            for (int row = 0; row < returnResult.GetLength(0); row++)
+            {
+                for (int col = 0; col < returnResult.GetLength(1); col++)
+                {
+                    final[row, col] = returnResult[row, col];
+                }
+            }
+
+            return final;
         }
 
         private double[,] matrixMultiplier(double[,] matrixA, double[,] matrixB) 
@@ -94,13 +130,30 @@ namespace fifi.Core
             {
                 for (int col = 0; col < matrixA.GetLength(1); col++)
                 {
-                    for (int inner = 0; inner < matrixA.GetLength(1); inner++)
+                    for (int inner = 0; inner < matrixA.GetLength(0); inner++)
                     {
                         resultMatrix[row, col] += matrixA[row, inner] * matrixB[inner, col];
                     }
                 }
             }
             return resultMatrix;
+        }
+
+        private double[,] identityMatrixGenerator(int size)
+        {
+            double[,] identityMatrix = new double[size, size];
+
+            for (int row = 0; row < squaredMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < squaredMatrix.GetLength(1); col++)
+                {
+                    if (row == col)
+                        identityMatrix[row, col] = 1;
+                    else
+                        identityMatrix[row, col] = 0;
+                }
+            }
+            return identityMatrix;
         }
     }
 }
