74741dddc728f9c8ad1d02f6dfbdf901e06658a6
Omar Ali Sheikh-Omar <omar@keepfocus.dk>
2015-04-21 09:02:15 +0200

    Make DataPoint more general.

--- src/app/fifi.Core/DataPoint.cs
+++ src/app/fifi.Core/DataPoint.cs
@@ -8,12 +8,44 @@ namespace fifi.Core
 {
     public class DataPoint
     {
-        public List<double> Values { get; private set; }
-        public DataItem Item { get; set; }
-
-        public DataPoint()
         {
-            Values = new List<double>();
+        }
+
+        public double[] Coordinates { get; private set; }
+
+
+        public double this[int index]
+        {
+            get { return Coordinates[index]; }
+            set { Coordinates[index] = value; }
+        }
+
+        /// <summary>
+        /// Copies all elements from another DataPoint.
+        /// </summary>
+        public void CopyFrom(DataPoint another)
+        {
+
+                this[i] = another[i];
+        }
+
+        public override bool Equals(object obj)
+        {
+            DataPoint other = obj as DataPoint;
+            if (other != null)
+            {
+                {
+                    {
+                        if (other[i] != this[i])
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
