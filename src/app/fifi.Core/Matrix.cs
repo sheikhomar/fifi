6c94ccea0e7173d8d6f2dd9987a5110a47fecb3c
stelle11 <stellehj@yahoo.dk>
2015-04-21 13:34:16 +0200

    Added two new classes

---
+++ src/app/fifi.Core/Matrix.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fifi.Core
+{
+    class Matrix
+    {
+        double[,] matrix;
+
+        public Matrix(int row, int colum)
+        {
+            matrix = new double[row, colum];
+        }
+
+        public double this[int row, int colum]
+        {
+            get {return matrix[row, colum]; }
+            set {matrix[row, colum] = value; }
+        }
+
+        public int GetLengthOfDim(int dimension, double[,] defaultMatrix)
+        {
+            return defaultMatrix.GetLength(dimension);
+        }
+
+        public int FirstDimension 
+        {
+            get { return matrix.GetLength(0); } 
+        }
+
+        public int SecondDimension
+        {
+            get { return matrix.GetLength(1); }
+        }
+
+        public void SquaredValues()
+        {
+            for (int row = 0; row < FirstDimension; row++)
+            {
+                for (int col = 0; col < SecondDimension; col++)
+                {
+                    matrix[row, col] *= matrix[row, col];
+                }
+            }
+        }
+
+        public Matrix IdentityMatrixGenerator(int size)
+        {
+            Matrix customMatrix = new Matrix(size, size);
+
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    if (row == col)
+                        customMatrix[row, col] = 1;
+                    else
+                        customMatrix[row, col] = 0;
+                }
+            }
+            return customMatrix;
+        }
+
+        public Matrix OnesMatrixGenerator(int size)
+        {
+            Matrix customMatrix = new Matrix(size, size);
+
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    customMatrix[row, col] = 1;
+                }
+            }
+            return customMatrix;
+        }
+    }
+}
---
+++ src/app/fifi.Core/MultiDimensionalScaling2.cs
@@ -0,0 +1,207 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using MathNet.Numerics.LinearAlgebra;
+//using MathNet.Numerics.LinearAlgebra.Double;
+//using MathNet.Numerics.LinearAlgebra.Factorization;
+//using System.Windows.Forms.DataVisualization.Charting;
+//using CDataPoint = System.Windows.Forms.DataVisualization.Charting.DataPoint;
+
+//namespace fifi.Core
+//{
+//    public class MultiDimensionalScaling
+//    {
+//        double[,] distanceMatrix;
+//        double[,] squaredMatrix;
+//        double[,] jMatrix;
+//        double[,] scalarProductMatrix;
+//        double[,] result;
+//        List<CDataPoint> L = new List<CDataPoint>();
+//        Matrix matrix;
+
+//        public MultiDimensionalScaling(double[,] data)
+//        {
+//            matrix = new Matrix(data);
+//            //distanceMatrix = data;
+//        }
+
+
+
+//        public double[,] Calculate()
+//        {
+//            matrix.SquaredValues();
+//            Matrix jMatrix = JMatrixGenerator();
+//            /*squaredMatrix = squaredDistanceMatrix(distanceMatrix);
+//            jMatrix = jMatrixCalculator(squaredMatrix);
+//            scalarProductMatrix = scalarProductMatrixCalculator(squaredMatrix, jMatrix);
+//            result = eigenCalculator(scalarProductMatrix);*/
+//            return result;
+//        }
+
+//        private Matrix JMatrixGenerator()
+//        {
+//            double dimensionScaling = Math.Pow(matrix.FirstDimension, -1);
+//            Matrix JMatrix = new Matrix(matrix.FirstDimension, matrix.SecondDimension);
+//            Matrix identityMatrix = matrix.IdentityMatrixGenerator(matrix.FirstDimension);
+//            Matrix oneMatrix = matrix.OnesMatrixGenerator(matrix.FirstDimension);
+
+//            for (int row = 0; row < matrix.FirstDimension; row++)
+//            {
+//                for (int col = 0; col < matrix.SecondDimension; col++)
+//                {
+//                    JMatrix[row, col] = identityMatrix[row, col] - (oneMatrix[row, col] * dimensionScaling);
+//                }
+//            }
+//            return JMatrix;
+//        }
+
+//        /*private double[,] squaredDistanceMatrix(double[,] distanceMatrix)
+//        {
+//            for (int row = 0; row < distanceMatrix.GetLength(0); row++)
+//            {
+//                for (int col = 0; col < distanceMatrix.GetLength(1); col++)
+//                {
+//                    distanceMatrix[row, col] *= distanceMatrix[row, col];
+//                }
+//            }
+//            return distanceMatrix;
+//        }*/
+
+//        /*private double[,] jMatrixCalculator(double[,] squaredMatrix)
+//        {
+//            double[,] identityMatrix = identityMatrixGenerator(squaredMatrix.GetLength(1));
+//            double[,] tempJMatrix = new double[squaredMatrix.GetLength(0), squaredMatrix.GetLength(1)];
+//            double[,] oneMatrix = new double[squaredMatrix.GetLength(0), squaredMatrix.GetLength(1)];
+//            double dimensionScaling = Math.Pow(squaredMatrix.GetLength(0), -1);
+
+//            for (int row = 0; row < squaredMatrix.GetLength(0); row++)
+//            {
+//                for (int col = 0; col < squaredMatrix.GetLength(1); col++)
+//                {
+//                    oneMatrix[row, col] = 1;
+//                }
+//            }
+
+//            for (int row = 0; row < squaredMatrix.GetLength(0); row++)
+//            {
+//                for (int col = 0; col < squaredMatrix.GetLength(1); col++)
+//                {
+//                    tempJMatrix[row, col] = identityMatrix[row, col] - (oneMatrix[row, col] * dimensionScaling);
+//                }
+//            }
+//            return tempJMatrix;
+//        }*/
+
+//        private double[,] scalarProductMatrixCalculator(double[,] squaredMatrix, double[,] jMatrix)
+//        {
+//            double[,] tempScalarProductMatrix = new double[squaredMatrix.GetLength(0), squaredMatrix.GetLength(1)];
+//            double[,] tempArray = new double[squaredMatrix.GetLength(0), squaredMatrix.GetLength(1)];
+
+//            for (int row = 0; row < squaredMatrix.GetLength(0); row++)
+//            {
+//                for (int col = 0; col < squaredMatrix.GetLength(1); col++)
+//                {
+//                    tempScalarProductMatrix[row, col] = -(0.5) * jMatrix[row, col];
+//                }
+//            }
+//            tempArray = matrixMultiplier(tempScalarProductMatrix, squaredMatrix);
+//            tempScalarProductMatrix = matrixMultiplier(tempArray, jMatrix);
+//            return tempScalarProductMatrix;
+//        }
+
+//        private Tuple<int, int> findLargestValueIndex(double[] valueArray)
+//        {
+//            int largestValue = 0, secondLargestValue = 1;
+//            int counter = 1;
+//            while (counter < valueArray.Length)
+//            {
+//                if (valueArray[counter] > valueArray[secondLargestValue])
+//                    secondLargestValue = counter;
+//                else if (valueArray[counter] >= valueArray[largestValue])
+//                {
+//                    secondLargestValue = largestValue;
+//                    largestValue = counter;
+//                }
+//                counter++;
+//            }
+//            Tuple<int, int> result = new Tuple<int, int>(largestValue, secondLargestValue);
+//            return result;
+//        }
+
+//        private double[,] eigenCalculator(double[,] scalarProductMatrix)
+//        {
+//            var convertedMatrix = DenseMatrix.OfArray(scalarProductMatrix);
+//            var eigen = convertedMatrix.Evd();
+
+//            double[] valueArray = eigen.EigenValues.Select(x => x.Real).ToArray();
+//            Tuple<int, int> largestValueIndex = findLargestValueIndex(valueArray);
+//            double[,] valueResult = { { Math.Sqrt(valueArray[largestValueIndex.Item1]), 0 }, { 0, Math.Sqrt(valueArray[largestValueIndex.Item2]) } };
+
+//            double[,] vectorArray = eigen.EigenVectors.ToArray();
+//            double[,] vectorResult = new double[2, scalarProductMatrix.GetLength(0)];
+
+//            for (int row = 0; row < vectorArray.GetLength(0); row++)
+//            {
+//                for (int col = 0; col < vectorArray.GetLength(0); col++)
+//                {
+//                    if (largestValueIndex.Item1 == col)
+//                        vectorResult[0, row] = vectorArray[row, col];
+//                    else if (largestValueIndex.Item2 == col)
+//                        vectorResult[1, row] = vectorArray[row, col];
+//                }
+//            }
+
+//            var valueResultMatrix = DenseMatrix.OfArray(valueResult);
+//            var vectorResultMatrix = DenseMatrix.OfArray(vectorResult);
+//            var ResultMatrix = valueResultMatrix.Multiply(vectorResultMatrix);
+//            double[,] returnResult = ResultMatrix.ToArray();
+//            double[,] final = new double[returnResult.GetLength(0), returnResult.GetLength(1)];
+
+//            for (int row = 0; row < returnResult.GetLength(0); row++)
+//            {
+//                for (int col = 0; col < returnResult.GetLength(1); col++)
+//                {
+//                    final[row, col] = returnResult[row, col];
+//                }
+//            }
+
+//            return final;
+//        }
+
+//        private double[,] matrixMultiplier(double[,] matrixA, double[,] matrixB)
+//        {
+//            double[,] resultMatrix = new double[matrixA.GetLength(0), matrixA.GetLength(1)];
+
+//            for (int row = 0; row < matrixA.GetLength(0); row++)
+//            {
+//                for (int col = 0; col < matrixA.GetLength(1); col++)
+//                {
+//                    for (int inner = 0; inner < matrixA.GetLength(0); inner++)
+//                    {
+//                        resultMatrix[row, col] += matrixA[row, inner] * matrixB[inner, col];
+//                    }
+//                }
+//            }
+//            return resultMatrix;
+//        }
+
+//        /*private double[,] identityMatrixGenerator(int size)
+//        {
+//            double[,] identityMatrix = new double[size, size];
+
+//            for (int row = 0; row < squaredMatrix.GetLength(0); row++)
+//            {
+//                for (int col = 0; col < squaredMatrix.GetLength(1); col++)
+//                {
+//                    if (row == col)
+//                        identityMatrix[row, col] = 1;
+//                    else
+//                        identityMatrix[row, col] = 0;
+//                }
+//            }
+//            return identityMatrix;
+//        }*/
+//    }
+//}
