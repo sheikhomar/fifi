17ceace13457631cdbc2a8ebf925405c98020a2d
Marc Beuchert <marcbeuchert@hotmail.com>
2015-04-21 10:48:38 +0200

    OutlierDetection Lavet, tilføjet Outlier klasse

--- src/app/fifi.Core/TempOutlierDetection.cs
+++ src/app/fifi.Core/TempOutlierDetection.cs
@@ -3,10 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using fifi.Core.Algorithms;
+using fifi.Core;
 
 namespace fifi.Core
 {
     class TempOutlierDetection
     {
+        ClusteringResult input;
+        double AverageDistance = 0;
+        List<Outlier> outlierList = new List<Outlier>();
+
+        public TempOutlierDetection(ClusteringResult Input)
+        {
+            input = Input;
+        }
+
+        public List<Outlier> Calculate()
+        {
+            foreach (var Cluster in input.Clusters)
+            {
+                FindOutliers(Cluster);
+            }
+            return outlierList;
+        }
+        
+        private double FindAverageDistance(Cluster Cluster)
+        {
+            foreach (var Member in Cluster.Members)
+            {
+                AverageDistance += Member.Distance;
+            }
+            return AverageDistance;
+        }
+
+        private void FindOutliers(Cluster Cluster)
+        {
+            AverageDistance = FindAverageDistance(Cluster);
+
+            foreach (var Member in Cluster.Members)
+	        {
+		        if (Member.Distance > AverageDistance) 
+                {
+                    outlierList.Add(new Outlier(Member, Cluster.Centroid));
+                }
+	        }
+        }
     }
 }
