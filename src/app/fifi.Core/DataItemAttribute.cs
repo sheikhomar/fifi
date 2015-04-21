74c8228a53cccb9f315945e125351af9e73d7ae6
Omar Ali Sheikh-Omar <omar@keepfocus.dk>
2015-04-21 09:16:34 +0200

    Introduce IdentifiableDataPoint.

---
+++ src/app/fifi.Core/DataItemAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fifi.Core
+{
+    public class DataItemAttribute
+    {
+        public DataItemAttribute(string name, double value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public string Name { get; private set; }
+        public double Value { get; private set; }
+    }
+}
---
+++ src/app/fifi.Core/IdentifiableDataPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fifi.Core
+{
+    public class IdentifiableDataPoint : DataPoint
+    {
+        public IdentifiableDataPoint(int id, int dimensions) : base(dimensions)
+        {
+            Id = id;
+            Attributes = new List<DataItemAttribute>();
+        }
+
+        public int Id { get; private set; }
+        public IList<DataItemAttribute> Attributes { get; private set; }
+    }
+}
