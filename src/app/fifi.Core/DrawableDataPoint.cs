c87e2ad0faef8566a802eb187e5ddaacd8c4d7de
Omar Ali Sheikh-Omar <omar@keepfocus.dk>
2015-04-22 09:44:58 +0200

    Introduce DrawableDataPoint.

---
+++ src/app/fifi.Core/DrawableDataPoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using fifi.Core.Algorithms;
+
+namespace fifi.Core
+{
+    public class DrawableDataPoint : DataPoint
+    {
+        public DrawableDataPoint(Cluster cluster, double x, double y) : base(2)
+        {
+            Cluster = cluster;
+            Group = string.Format("Cluster {0}", cluster.Id);
+            X = x;
+            Y = y;
+        }
+
+        public Cluster Cluster { get; private set; }
+        public string Group { get; private set; }
+
+        public double X
+        {
+            get { return this[0]; }
+            set { this[0] = value; }
+        }
+
+        public double Y
+        {
+            get { return this[1]; }
+            set { this[1] = value; }
+        }
+    }
+}
