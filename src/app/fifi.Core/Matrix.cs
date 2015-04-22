f70b4411096f729753293a0581a5d6b9a4f7634d
stelle11 <stellehj@yahoo.dk>
2015-04-22 12:40:34 +0200

    MDS fix

--- src/app/fifi.Core/Matrix.cs
+++ src/app/fifi.Core/Matrix.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MathNet.Numerics.LinearAlgebra;
-using MathNet.Numerics.LinearAlgebra.Double;
-using MathNet.Numerics.LinearAlgebra.Factorization;
 
 namespace fifi.Core
 {
@@ -30,11 +27,6 @@ namespace fifi.Core
             set { matrix = value; }
         }
 
-        public int GetLengthOfDim(int dimension, double[,] defaultMatrix)
-        {
-            return defaultMatrix.GetLength(dimension);
-        }
-
         public int FirstDimension 
         {
             get { return matrix.GetLength(0); } 
@@ -54,66 +46,6 @@ namespace fifi.Core
                     matrix[row, col] *= matrix[row, col];
                 }
             }
-        }
-
-        public Matrix EigenGenerator()
-        {
-            var convertedMatrix = DenseMatrix.OfArray(matrix);
-            var eigenInfo = convertedMatrix.Evd();
-
-            double[] eigenvalueArray = eigenInfo.EigenValues.Select(x => x.Real).ToArray();
-            Tuple<int, int> largestTwoEigenvalues = FindLargestTwoEigenvalues(eigenvalueArray);
-            //double[,] eigenvalueMatrix2 = { { Math.Sqrt(largestEigenvalues.Item1), 0 }, { 0, Math.Sqrt(largestEigenvalues.Item2) } };
-            double[] eigenvalueMatrixInput = { Math.Sqrt(eigenvalueArray[largestTwoEigenvalues.Item1]), 0, 0, Math.Sqrt(eigenvalueArray[largestTwoEigenvalues.Item2]) };
-            Matrix eigenvalueMatrix = GenerateFullSquaredCustomMatrix(eigenvalueMatrixInput);
-            
-            double[,] vectorArray = eigenInfo.EigenVectors.ToArray();
-            double[,] vectorResult = new double[2, matrix.GetLength(1)];
-
-            for (int row = 0; row < vectorArray.GetLength(0); row++)
-            {
-                for (int col = 0; col < vectorArray.GetLength(0); col++)
-                {
-                    if (largestTwoEigenvalues.Item1 == col)
-                        vectorResult[0, row] = vectorArray[row, col];
-                    else if (largestTwoEigenvalues.Item2 == col)
-                        vectorResult[1, row] = vectorArray[row, col];
-                }
-            }
-
-            var valueResultMatrix = DenseMatrix.OfArray(eigenvalueMatrix.GetSetMatrix);
-            var vectorResultMatrix = DenseMatrix.OfArray(vectorResult);
-            var ResultMatrix = valueResultMatrix.Multiply(vectorResultMatrix);
-            double[,] returnResult = ResultMatrix.ToArray();
-            Matrix final = new Matrix(returnResult.GetLength(0), returnResult.GetLength(1));
-
-            for (int row = 0; row < returnResult.GetLength(0); row++)
-            {
-                for (int col = 0; col < returnResult.GetLength(1); col++)
-                {
-                    final[row, col] = returnResult[row, col];
-                }
-            }
-            return final;
-        }
-
-        private Tuple<int, int> FindLargestTwoEigenvalues(double[] valueArray)
-        {
-            int largestValue = 0, secondLargestValue = 1;
-            int counter = 1;
-            while (counter < valueArray.Length)
-            {
-                if (valueArray[counter] > valueArray[secondLargestValue])
-                    secondLargestValue = counter;
-                else if (valueArray[counter] >= valueArray[largestValue])
-                {
-                    secondLargestValue = largestValue;
-                    largestValue = counter;
-                }
-                counter++;
-            }
-            Tuple<int, int> result = new Tuple<int, int>(largestValue, secondLargestValue);
-            return result;
         }
 
         public Matrix IdentityMatrixGenerator(int size)
--- src/app/fifi.Core/MultiDimensionalScaling2.cs
+++ src/app/fifi.Core/MultiDimensionalScaling2.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.LinearAlgebra.Factorization;
 
 namespace fifi.Core
 {
@@ -21,7 +24,7 @@ namespace fifi.Core
             matrix.SquaredValues();
             Matrix jMatrix = JMatrixGenerator();
             Matrix scalarProductMatrix = ScalarProductMatrixGenerator(jMatrix);
-            return scalarProductMatrix.EigenGenerator().GetSetMatrix;
+            return TwoDCoordinateGenerator(scalarProductMatrix).GetSetMatrix;
         }
 
         private Matrix JMatrixGenerator()
@@ -55,6 +58,55 @@ namespace fifi.Core
             resultMatrix = MatrixMultiplier(resultMatrix, matrix);
             resultMatrix = MatrixMultiplier(resultMatrix, jMatrix);
             return resultMatrix;
+        }
+
+        private Matrix TwoDCoordinateGenerator(Matrix scalarProductMatrix)
+        {
+            Matrix<double> convertedMatrix = DenseMatrix.OfArray(scalarProductMatrix.GetSetMatrix);
+            var eigenInfo = convertedMatrix.Evd();
+
+            /* Eigenvalue calculator */
+            double[] eigenvalueArray = eigenInfo.EigenValues.Select(x => x.Real).ToArray();
+            Tuple<int, int> largestTwoEigenvalues = FindLargestTwoEigenvalues(eigenvalueArray);
+            double[,] eigenvalueMatrixUnconverted = { { Math.Sqrt(eigenvalueArray[largestTwoEigenvalues.Item1]), 0 }, { 0, Math.Sqrt(eigenvalueArray[largestTwoEigenvalues.Item2]) } };
+            Matrix<double> eigenvalueMatrix = Matrix<double>.Build.DenseOfArray(eigenvalueMatrixUnconverted);
+
+            /* Eigenvector calculator */
+            Matrix<double> eigenvectorMatrix = Matrix<double>.Build.Dense(2, matrix.SecondDimension);
+            double[] firstEigenvector = eigenInfo.EigenVectors.Column(largestTwoEigenvalues.Item1).ToArray();
+            double[] secondEigenvector = eigenInfo.EigenVectors.Column(largestTwoEigenvalues.Item2).ToArray();
+            double[] eigenvector = firstEigenvector;
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < eigenvectorMatrix.ColumnCount; col++)
+                {
+                    eigenvectorMatrix[row, col] = eigenvector[col];
+                }
+                eigenvector = secondEigenvector;
+            }
+
+            Matrix resultMatrix = new Matrix(eigenvalueMatrix.RowCount, eigenvectorMatrix.ColumnCount);
+            resultMatrix.GetSetMatrix = eigenvalueMatrix.Multiply(eigenvectorMatrix).ToArray();
+            return resultMatrix;
+        }
+
+        private Tuple<int, int> FindLargestTwoEigenvalues(double[] valueArray)
+        {
+            int largestValue = 0, secondLargestValue = 1;
+            int counter = 1;
+            while (counter < valueArray.Length)
+            {
+                if (valueArray[counter] > valueArray[secondLargestValue])
+                    secondLargestValue = counter;
+                else if (valueArray[counter] >= valueArray[largestValue])
+                {
+                    secondLargestValue = largestValue;
+                    largestValue = counter;
+                }
+                counter++;
+            }
+            Tuple<int, int> result = new Tuple<int, int>(largestValue, secondLargestValue);
+            return result;
         }
 
         private Matrix MatrixMultiplier(Matrix firstMatrix, Matrix secondMatrix)
