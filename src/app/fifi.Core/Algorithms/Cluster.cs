6d423cc2e8980037f97aa0c945901c1f06db03a5
Omar Ali Sheikh-Omar <omar@keepfocus.dk>
2015-04-22 09:39:49 +0200

    Give each cluster unique ID.

--- src/app/fifi.Core/Algorithms/Cluster.cs
+++ src/app/fifi.Core/Algorithms/Cluster.cs
@@ -8,13 +8,17 @@ namespace fifi.Core.Algorithms
 {
     public class Cluster
     {
+        private static int _id = 1;
+
         public Cluster(Centroid centroid)
         {
             Centroid = centroid;
             Members = new List<ClusterMember>();
+            Id = _id++;
         }
 
         public Centroid Centroid { get; private set; }
         public IList<ClusterMember> Members { get; private set; }
+        public int Id { get; private set; }
     }
 }
