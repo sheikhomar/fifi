4a09ed977c329c8b637d23647ea0d4984fe04319
MBSource <mathiasbsvendsen@gmail.com>
2015-04-22 16:20:54 +0200

    More tests for Euc Dist Test - WIP

--- src/test/fifi.Tests/Core/Algorithms/EuclideanMetricTests.cs
+++ src/test/fifi.Tests/Core/Algorithms/EuclideanMetricTests.cs
@@ -49,7 +49,7 @@ namespace fifi.Tests.Core.Algorithms
         }
 
         [Test]
-        public void CalculateWithSwappingTheCoordinates()
+        public void CalculateWithSwappingCoordinates()
         {
             var coordinatesA = new double[] { 0, 0, 0, 0, 0 };
             dataPointA = new DataPoint(coordinatesA);
@@ -59,41 +59,14 @@ namespace fifi.Tests.Core.Algorithms
             var result = metric.Calculate(dataPointA, dataPointB) - metric.Calculate(dataPointB, dataPointA);
             Assert.AreEqual(0D, result);
         }
-        //private void convertFromStringToDataPoint(DataPoint dataPoint, string[] stringWithCoordinates)
-        //{
-        //    double[] coordinates = new double[dimensions];
-        //    for (int index = 0; index < dimensions; index++)
-        //    {
-        //        coordinates[index] = Convert.ToDouble(stringWithCoordinates[index]);
-        //    }
-        //    dataPoint.SetCoordinates(coordinates);
-        //}
 
+
+        //[Test]
+        //public void 
+      
         //test ideas:
         // close to 0, close to max, medium results, swapping A and B should yeild the same, differnt array size (this will result in an error)
         //Notes: I somehow need to make the below coordinate definition work with our test runs... Several people suggest TestCaseSource - but I can't get a result with that?!
 
-
-
-        //Please make this match the dimensions
-        //public static double[] coorZero = { 0.0, 0.0, 0.0, 0.0, 0.0 };
-        //public static double[] coorA = { 1, 2, 3, 5, 5 };
-        //public static double[] coorB = { 1, 1, 1, 1, 1 };
-        //public static double[] coorC = { 0.0, 0.0, 0.0, 0.0, 0.0 };
-        //public static double[] coorD = { 0.0, 0.0, 0.0, 0.0, 0.0 };
-        //public static double[] coorE = { 0.0, 0.0, 0.0, 0.0, 0.0 };
-        //public static double[] coorF = { 0.0, 0.0, 0.0, 0.0, 0.0 };
-
-        //[TestCase(coorZero, coorZero, ExpectedResult = 0)]
-        //[TestCase(coorA, coorA, ExpectedResult = 0)]
-        //[TestCase(coorZero, coorB, ExpectedResult = 1)]
-        //public double TestEuclideanMetric_DataPoint(double[] pointACoordinates, double[] pointBCoordinates)
-        //{
-        //    //Convert from double to dataPoint
-        //    dataPointA.SetCoordinates(pointACoordinates);
-        //    dataPointA.SetCoordinates(pointBCoordinates);
-
-        //    return metric.Calculate(dataPointA, dataPointB); ;
-        //}
     }
 }
