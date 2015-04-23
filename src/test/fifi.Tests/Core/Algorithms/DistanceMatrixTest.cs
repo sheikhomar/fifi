8c4d714e1e5e65c2c6e1a83ddbc91d175e159258
stelle11 <stellehj@yahoo.dk>
2015-04-23 09:46:26 +0200

    DistanceMatrixTest added

---
+++ src/test/fifi.Tests/Core/Algorithms/DistanceMatrixTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using fifi.Core;
+
+namespace fifi.Tests.Core.Algorithms
+{
+    [TestFixture]
+    class DistanceMatrixTest
+    {
+
+        [Test]
+        public void sometest()
+        {
+
+        }
+    }
+}
