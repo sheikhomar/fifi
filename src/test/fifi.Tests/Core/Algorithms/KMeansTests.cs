f884ccc8f734f526a0d917436484ced4b2be1859
JakobKoch <koch.jakob.94@gmail.com>
2015-04-23 10:04:05 +0200

    KMeansTest dataCollection completed. WIP
    all Values for test dataCollection has ben added.

--- src/test/fifi.Tests/Core/Algorithms/KMeansTests.cs
+++ src/test/fifi.Tests/Core/Algorithms/KMeansTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using fifi.Core;
@@ -44,12 +45,54 @@ namespace fifi.Tests.Core.Algorithms
             for (int i = 0; i < 9; i++)
             {
                 dataCollection[i].AddAttribute("Gender", 1d);
+                if (i < 4)
+                {
+                    dataCollection[i].AddAttribute("Income", 1d);
+                }
+                else
+                {
+                    dataCollection[i].AddAttribute("Income", 0.2858d);
+                }
+                dataCollection[i].AddAttribute("Age", 0.16d);
+                if (i < 2 || i > 3)
+                {
+                    dataCollection[i].AddAttribute("Purchase", 1d);
+                }
+                else
+                {
+                    dataCollection[i].AddAttribute("Purchase", 0.5d);
+                }
+                if (i < 2)
+                {
+                    dataCollection[i].AddAttribute("Control", 0.5d);
+                }
+                else
+                {
+                    dataCollection[i].AddAttribute("Control", 1d);
+                }
+                
             }
 
             dataCollection[9].AddAttribute("Gender", 0d);
+            dataCollection[9].AddAttribute("Income", 0.1429d);
+            dataCollection[9].AddAttribute("Age", 0.16d);
+            dataCollection[9].AddAttribute("Purchase", 1d);
+            dataCollection[9].AddAttribute("Control", 0d);
 
             Assert.AreEqual(dataCollection[0].Attributes[0].Value, dataCollection[1].Attributes[0].Value);
 
+        }
+
+        [Test]
+        public void CentroidsArePlacedCorrect()
+        {
+            
+        }
+
+        [Test]
+        public void DataPointsAreAssignedToCentroids()
+        {
+            
         }
     }
 }
