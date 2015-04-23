bd321987f388612ba65030238680e0100513f954
JakobKoch <koch.jakob.94@gmail.com>
2015-04-23 16:12:32 +0200

    Added ArgumentException to Kmeans (0 centroids)

--- src/app/fifi.Core/Algorithms/KMeans.cs
+++ src/app/fifi.Core/Algorithms/KMeans.cs
@@ -28,6 +28,11 @@ namespace fifi.Core.Algorithms
                 .Take(k)
                 .Select(dataPoint => dataPoint.Copy())
                 .ToList();
+            if (k > 1)
+            {
+                throw new ArgumentException("Clusters cannot be generated for less than one centroid");
+            }
+
         }
 
         public KMeans(IdentifiableDataPointCollection dataCollection, int[] centroidIndicies, IDistanceMetric distanceMetric, int maxIterations = 100)
@@ -135,5 +140,6 @@ namespace fifi.Core.Algorithms
 
             return gravityCenter;
         }
+
     }
 }
