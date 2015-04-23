20282cf1d64ca5783851028a24a94280a8844c2e
JakobKoch <koch.jakob.94@gmail.com>
2015-04-23 08:38:10 +0200

    removed useless comments from KMeans.Test. WIP

--- src/test/fifi.Tests/Core/Algorithms/KMeansTests.cs
+++ src/test/fifi.Tests/Core/Algorithms/KMeansTests.cs
@@ -35,45 +35,17 @@ namespace fifi.Tests.Core.Algorithms
             int dimentions = 5;
 
             dataCollection = new IdentifiableDataPointCollection();
-            //var dataPointB = new IdentifiableDataPoint(id++, dimentions);
-            //var dataPointC = new IdentifiableDataPoint(id++, dimentions);
-            //var dataPointD = new IdentifiableDataPoint(id++, dimentions);
-            //var dataPointE = new IdentifiableDataPoint(id++, dimentions);
-            //var dataPointF = new IdentifiableDataPoint(id++, dimentions);
-            //var dataPointG = new IdentifiableDataPoint(id++, dimentions);
-            //var dataPointH = new IdentifiableDataPoint(id++, dimentions);
-            //var dataPointI = new IdentifiableDataPoint(id++, dimentions);
-            //var dataPointJ = new IdentifiableDataPoint(id++, dimentions);
             for (int i = 0; i < 10; i++)
             {
                 dataPoint = new IdentifiableDataPoint(id++, dimentions);
                 dataCollection.AddItem(dataPoint);
             }
-            //dataCollection.AddItem(dataPointA);
-            //dataCollection.AddItem(dataPointB);
-            //dataCollection.AddItem(dataPointC);
-            //dataCollection.AddItem(dataPointD);
-            //dataCollection.AddItem(dataPointE);
-            //dataCollection.AddItem(dataPointF);
-            //dataCollection.AddItem(dataPointG);
-            //dataCollection.AddItem(dataPointH);
-            //dataCollection.AddItem(dataPointI);
-            //dataCollection.AddItem(dataPointJ);
 
             for (int i = 0; i < 9; i++)
             {
                 dataCollection[i].AddAttribute("Gender", 1d);
             }
 
-            //dataCollection[0].AddAttribute("Gender", 1d);
-            //dataCollection[1].AddAttribute("Gender", 1d);
-            //dataCollection[2].AddAttribute("Gender", 1d);
-            //dataCollection[3].AddAttribute("Gender", 1d);
-            //dataCollection[4].AddAttribute("Gender", 1d);
-            //dataCollection[5].AddAttribute("Gender", 1d);
-            //dataCollection[6].AddAttribute("Gender", 1d);
-            //dataCollection[7].AddAttribute("Gender", 1d);
-            //dataCollection[8].AddAttribute("Gender", 1d);
             dataCollection[9].AddAttribute("Gender", 0d);
 
             Assert.AreEqual(dataCollection[0].Attributes[0].Value, dataCollection[1].Attributes[0].Value);
