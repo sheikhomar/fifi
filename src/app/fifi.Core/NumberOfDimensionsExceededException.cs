4af0e0592c9aadd1848ffb80b61bfd314e74b5e2
JakobKoch <koch.jakob.94@gmail.com>
2015-04-23 18:40:49 +0200

    Created datapointtest. WIP

---
+++ src/app/fifi.Core/NumberOfDimensionsExceededException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fifi.Core
+{
+    public class NumberOfDimensionsExceededException : Exception
+    {
+        public NumberOfDimensionsExceededException(string message) : base(message)
+        {
+            
+        }
+    }
+}
---
+++ src/test/fifi.Tests/Core/DataPointTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using fifi.Core;
+using NUnit.Framework;
+
+namespace fifi.Tests.Core
+{
+    [TestFixture]
+    public class IdentifiableDataPointTests
+    {
+        [Test]
+        public void AddAttributesShouldCheckBounds()
+        {
+            IdentifiableDataPoint _datapoint = new IdentifiableDataPoint(0, 1);
+
+            _datapoint.AddAttribute("Hej", 27);
+
+            Assert.Throws<NumberOfDimensionsExceededException>(() => _datapoint.AddAttribute("", 0));
+        }
+
+    }
+}
