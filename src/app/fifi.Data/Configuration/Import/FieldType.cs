21ce51b5eb39dbb3718e14bb08a6a37a56d1d4c1
Omar Ali Sheikh-Omar <omar@keepfocus.dk>
2015-04-23 10:46:00 +0200

    Introduce FieldType.

---
+++ src/app/fifi.Data/Configuration/Import/FieldType.cs
@@ -0,0 +1,10 @@
+namespace fifi.Data.Configuration.Import
+{
+    public enum FieldType
+    {
+        BinaryValue,
+        MultipleBinaryFields,
+        MultipleChoiceMultipleBinaryFields,
+        NumericField
+    }
+}
