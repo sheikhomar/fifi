2dc196abaeff83da54d448d528422d7d0bb2cbb5
Marc Beuchert <marcbeuchert@hotmail.com>
2015-04-24 14:02:56 +0200

    WiP - Local Outlier Factor

---
+++ src/app/fifi.Core/LocalOutlierFactor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace fifi.Core
+{
+    public class LocalOutlierFactor
+    {
+        Matrix DistanceMatrix;
+        private int KNeighbours;
+        List<LocalOutlierFactorPoint> ResultList = new List<LocalOutlierFactorPoint>();
+
+        public LocalOutlierFactor(Matrix distanceMatrix, int kNeighbours)
+        {
+            DistanceMatrix = distanceMatrix;
+            KNeighbours = kNeighbours;
+        }
+
+        public void Run()
+        {
+            DistanceToKthNeighbour(DistanceMatrix);
+            LocalReachabilityDensity(KNeighbours);
+
+        }
+
+        private void DistanceToKthNeighbour(Matrix distanceMatrix)
+        {
+            int lengthDim1 = distanceMatrix.FirstDimension;
+            int lengthDim2 = distanceMatrix.SecondDimension;
+            
+            for (int row = 0; row < lengthDim1; row++)
+            {
+                LocalOutlierFactorPoint person = new LocalOutlierFactorPoint();
+                for (int col = 0; col < lengthDim2; col++)
+                {
+                    if (row != col)
+                        person.DistanceToNeighbours.Add(Tuple.Create<int, double>(col, DistanceMatrix.GetSetMatrix[row, col]));
+                }
+                person.DistanceToNeighbours.Sort();
+                person.DistanceToNeighbours = person.DistanceToNeighbours.Take(KNeighbours).ToList();
+                ResultList.Add(person);
+            }
+        }
+
+        private void LocalReachabilityDensity(int kNeighbours)
+        {
+            foreach (var person in ResultList)
+            {
+                double sumOfReachDistK = 0;
+
+                foreach (var id in person.DistanceToNeighbours)
+                {
+                    sumOfReachDistK += 
+                }
+            }
+        }
+
+        private void CalcReachDistK(LocalOutlierFactorPoint person)
+        {
+            
+        }
+
+        private void LocalOutlierFactor()
+        {
+            
+        }
+    }
+}
---
+++ src/app/fifi.Core/LocalOutlierFactorPoint.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace fifi.Core
+{
+    public class LocalOutlierFactorPoint
+    {
+        public List<Tuple<int, double>> DistanceToNeighbours = new List<Tuple<int, double>>();
+        public double LocalReachabilityDensity;
+    }
+}
