f7477c2bc2b39b2c3d5ea447b2d3d0056c5ce629
Omar Ali Sheikh <sheikhomar@gmail.com>
2015-04-26 15:04:19 +0200

    Add base class for all windows.

---
+++ src/app/fifi.WinUI/BaseForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fifi.WinUI
+{
+    public partial class BaseForm : Form
+    {
+        /// <summary>
+        /// Since UI controls can only be modified by the UI thread, we
+        /// need an SychronizationContext object for our Task.
+        /// Source: http://www.codeproject.com/Articles/152765/Task-Parallel-Library-of-n
+        /// </summary>
+        protected readonly TaskScheduler FormTaskScheduler;
+
+        public BaseForm()
+        {
+            InitializeComponent();
+
+            FormTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+        }
+    }
+}
