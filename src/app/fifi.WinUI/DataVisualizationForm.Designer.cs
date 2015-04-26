5adf9221a6da29847112daace7be4887a60993d1
Omar Ali Sheikh <sheikhomar@gmail.com>
2015-04-26 15:06:55 +0200

    Add DataVisualizationForm.

---
+++ src/app/fifi.WinUI/DataVisualizationForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace fifi.WinUI
+{
+    partial class DataVisualizationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.loadingImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loadingImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart
+            // 
+            this.chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart.BackImageAlignment = System.Windows.Forms.DataVisualization.Charting.ChartImageAlignmentStyle.Center;
+            this.chart.BackImageWrapMode = System.Windows.Forms.DataVisualization.Charting.ChartImageWrapMode.Unscaled;
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(11, 151);
+            this.chart.Margin = new System.Windows.Forms.Padding(2);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(893, 441);
+            this.chart.TabIndex = 1;
+            this.chart.Text = "chart1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(892, 131);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filter Settings";
+            // 
+            // loadingImage
+            // 
+            this.loadingImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.loadingImage.BackColor = System.Drawing.Color.White;
+            this.loadingImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.loadingImage.Location = new System.Drawing.Point(12, 151);
+            this.loadingImage.Name = "loadingImage";
+            this.loadingImage.Size = new System.Drawing.Size(26, 28);
+            this.loadingImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.loadingImage.TabIndex = 0;
+            this.loadingImage.TabStop = false;
+            // 
+            // DataVisualizationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 603);
+            this.Controls.Add(this.loadingImage);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chart);
+            this.Name = "DataVisualizationForm";
+            this.Text = "VisualizeForm";
+            this.Load += new System.EventHandler(this.DataVisualizationForm_Load);
+            this.Shown += new System.EventHandler(this.DataVisualizationForm_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loadingImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox loadingImage;
+    }
+}
---
+++ src/app/fifi.WinUI/DataVisualizationForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using fifi.Core;
+using fifi.Core.Algorithms;
+using fifi.WinUI.Properties;
+
+namespace fifi.WinUI
+{
+    public partial class DataVisualizationForm : BaseForm
+    {
+        private readonly IdentifiableDataPointCollection dataSet;
+        private readonly DataConversionTask dataConversionTask;
+        private readonly IDistanceMetric distanceMetric;
+
+        public DataVisualizationForm()
+        {
+            InitializeComponent();
+            this.loadingImage.Location = this.chart.Location;
+            this.loadingImage.Size = this.chart.Size;
+
+            // Loading chart stolen from https://dribbble.com/shots/1420523-Loading-Chart
+            this.loadingImage.Image = Resources.Loading;
+        }
+
+        public DataVisualizationForm(IdentifiableDataPointCollection dataSet, IDistanceMetric distanceMetric) : this()
+        {
+            this.dataSet = dataSet;
+            this.distanceMetric = distanceMetric;
+
+            dataConversionTask = new DataConversionTask();
+            dataConversionTask.Success += DataConversionTask_Success;
+            dataConversionTask.Failure += DataConversionTask_Failure;
+        }
+
+        private void DataVisualizationForm_Load(object sender, System.EventArgs e)
+        {
+        }
+
+        private void DataConversionTask_Success(object sender, IEnumerable<DrawableDataPoint> result)
+        {
+            loadingImage.Visible = false;
+            var scatterPlot = new ScatterPlot(result.ToList(), chart);
+            scatterPlot.Draw();
+
+            ChartArea area = chart.ChartAreas[0];
+
+            area.AxisX.Crossing = 0;
+            area.AxisY.Crossing = 0;
+        }
+
+        private void DataConversionTask_Failure(object sender, IEnumerable<Exception> errors)
+        {
+            loadingImage.Visible = false;
+            StringBuilder temp = new StringBuilder();
+            temp.AppendLine("Data import failed with following errors:");
+            foreach (var exp in errors)
+                temp.Append(" - ").AppendLine(exp.Message);
+
+            MessageBox.Show(temp.ToString(), "Data conversion failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void DataVisualizationForm_Shown(object sender, EventArgs e)
+        {
+            dataConversionTask.Start(dataSet, distanceMetric);
+        }
+    }
+}
