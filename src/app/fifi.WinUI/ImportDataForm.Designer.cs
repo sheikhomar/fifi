396ac15cb75b02b05b64a2c30e029ca4826ea542
Omar Ali Sheikh <sheikhomar@gmail.com>
2015-04-26 15:07:29 +0200

    Add ImportDataForm.

---
+++ src/app/fifi.WinUI/ImportDataForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace fifi.WinUI
+{
+    partial class ImportDataForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSelectedFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.chkRemoveWhitespace = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File";
+            // 
+            // txtSelectedFile
+            // 
+            this.txtSelectedFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSelectedFile.Location = new System.Drawing.Point(99, 33);
+            this.txtSelectedFile.Name = "txtSelectedFile";
+            this.txtSelectedFile.ReadOnly = true;
+            this.txtSelectedFile.Size = new System.Drawing.Size(255, 20);
+            this.txtSelectedFile.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(360, 31);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Colon",
+            "Semicolon",
+            "Tab"});
+            this.comboBox1.Location = new System.Drawing.Point(99, 60);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(336, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Field delimiter";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Value delimiter";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Colon",
+            "Semicolon",
+            "Tab"});
+            this.comboBox2.Location = new System.Drawing.Point(99, 87);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(336, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // chkRemoveWhitespace
+            // 
+            this.chkRemoveWhitespace.AutoSize = true;
+            this.chkRemoveWhitespace.Location = new System.Drawing.Point(94, 114);
+            this.chkRemoveWhitespace.Name = "chkRemoveWhitespace";
+            this.chkRemoveWhitespace.Size = new System.Drawing.Size(123, 17);
+            this.chkRemoveWhitespace.TabIndex = 7;
+            this.chkRemoveWhitespace.Text = "Remove whitespace";
+            this.chkRemoveWhitespace.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.txtSelectedFile);
+            this.groupBox1.Controls.Add(this.chkRemoveWhitespace);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnBrowse);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(460, 145);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Please select input file:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(372, 169);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(99, 31);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnImport.Location = new System.Drawing.Point(267, 169);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(99, 31);
+            this.btnImport.TabIndex = 10;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // LoadDataForm
+            // 
+            this.AcceptButton = this.btnImport;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(484, 212);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(500, 250);
+            this.MinimumSize = new System.Drawing.Size(500, 250);
+            this.Name = "LoadDataForm";
+            this.Text = "Select input data";
+            this.Load += new System.EventHandler(this.LoadDataForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSelectedFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.CheckBox chkRemoveWhitespace;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnImport;
+    }
+}
---
+++ src/app/fifi.WinUI/ImportDataForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using fifi.Core;
+using fifi.Core.Algorithms;
+using fifi.Data;
+using fifi.Data.Configuration.Import;
+
+namespace fifi.WinUI
+{
+    public partial class ImportDataForm : BaseForm
+    {
+        private class ImportOptions
+        {
+            public string Path { get; set; }
+            public string FieldDelimiter { get; set; }
+            public char ValueDelimiter { get; set; }
+            public bool RemoveWhiteSpace { get; set; }
+            public IConfiguration Configuration { get; set; }
+        }
+
+        public ImportDataForm()
+        {
+            InitializeComponent();
+        }
+
+        public IdentifiableDataPointCollection Result { get; private set; }
+
+        #region Event Handlers
+        private void LoadDataForm_Load(object sender, EventArgs e)
+        {
+            txtSelectedFile.Text = Path.Combine(Environment.CurrentDirectory, "UserData.csv");
+            btnBrowse.Select();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            ToggleUserInputControls(false);
+
+            ImportOptions importOptions = GetImportOptions();
+
+            var task = Task.Factory.StartNew<IdentifiableDataPointCollection>(DataImportTask_Run, importOptions);
+            task.ContinueWith(DataImportTask_Complete, FormTaskScheduler);
+            task.ContinueWith(DataImportTask_Faulted, CancellationToken.None, TaskContinuationOptions.OnlyOnFaulted, FormTaskScheduler);
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog {CheckFileExists = true};
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+                txtSelectedFile.Text = dialog.FileName;
+        }
+
+        private IdentifiableDataPointCollection DataImportTask_Run(object input)
+        {
+            ImportOptions options = input as ImportOptions;
+
+            var reader = new StreamReader(options.Path);
+            var importer = new CsvDynamicDataImporter(reader, options.Configuration)
+            {
+                FieldDelimiter = options.FieldDelimiter,
+                ValueDelimiter = options.ValueDelimiter,
+                RemoveWhiteSpace = options.RemoveWhiteSpace
+            };
+            return importer.Run();
+        }
+
+        private void DataImportTask_Complete(Task<IdentifiableDataPointCollection> task)
+        {
+            ToggleUserInputControls(true);
+
+            Result = task.Result;
+            MessageBox.Show("Data imported! Count=" + Result.Count, "Data import successful", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+
+            DataVisualizationForm form = new DataVisualizationForm(Result, new EuclideanMetric());
+            form.Show();
+        }
+
+        private void DataImportTask_Faulted(Task<IdentifiableDataPointCollection> task)
+        {
+            ToggleUserInputControls(true);
+
+            if (task.Exception != null)
+            {
+                StringBuilder temp = new StringBuilder();
+                temp.AppendLine("Data import failed with following errors:");
+                foreach (var exp in task.Exception.InnerExceptions)
+                    temp.Append(" - ").AppendLine(exp.Message);
+
+                MessageBox.Show(temp.ToString(), "Data import failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        private ImportOptions GetImportOptions()
+        {
+            IConfiguration configuration =
+                (ConfigurationSectionHandler)ConfigurationManager.GetSection("csvDataImport");
+
+            return new ImportOptions
+            {
+                Configuration = configuration,
+                Path = txtSelectedFile.Text,
+                FieldDelimiter = ",",
+                ValueDelimiter = ',',
+                RemoveWhiteSpace = chkRemoveWhitespace.Checked
+            };
+        }
+
+        private bool ValidateUserInput()
+        {
+            return true;
+        }
+
+        private void ToggleUserInputControls(bool enabled)
+        {
+            groupBox1.Enabled = enabled;
+            btnImport.Enabled = enabled;
+        }
+
+        #endregion
+    }
+}
