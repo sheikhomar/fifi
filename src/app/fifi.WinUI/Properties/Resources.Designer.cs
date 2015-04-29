22a033925b2531be037637a246e68831a583e08b
Omar Ali Sheikh <sheikhomar@gmail.com>
2015-04-29 21:10:23 +0200

    Add better loading image when generating chart.

--- src/app/fifi.WinUI/Properties/Resources.Designer.cs
+++ src/app/fifi.WinUI/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace fifi.WinUI.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Loading2 {
+            get {
+                object obj = ResourceManager.GetObject("Loading2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
