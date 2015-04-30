1768b1ccd2a71c09d1bc556e8a818ad0afddd89e
Marc Beuchert <marcbeuchert@hotmail.com>
2015-04-30 10:17:07 +0200

    Fik sgu' ikk lige commit ordentligt   ლ(ಥ Д ಥ )ლ

--- src/test/fifi.Tests/Core/LocalOutlierFactorTest.cs
+++ src/test/fifi.Tests/Core/LocalOutlierFactorTest.cs
@@ -1,7 +1,22 @@
+using System.Drawing.Drawing2D;
+using NUnit.Framework;
+
 namespace fifi.Data
 {
+    [TestFixture]
     public class LocalOutlierFactorTest
     {
-        
+        private Matrix distanceMatrix;
+        private int kNeighbors;
+
+        [SetUp]
+        public void SetUp()
+        {
+            distanceMatrix = new Matrix();
+            kNeighbors = 5;
+        }
+
+        [Test]
+        public void 
     }
 }
