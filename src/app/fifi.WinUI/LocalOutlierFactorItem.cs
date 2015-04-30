e2a4368b9861c4e3560c4aebb0c7f8b8dcbd086f
Omar Ali Sheikh-Omar <omar@keepfocus.dk>
2015-04-30 18:56:08 +0200

    Fix compiler error.

--- src/app/fifi.WinUI/LocalOutlierFactorItem.cs
+++ src/app/fifi.WinUI/LocalOutlierFactorItem.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace fifi.WinUI
 {
+    public enum IconType
+    {
+        Flag, Question, None
+    }
+
     public class LocalOutlierFactorItem
     {
-        enum Icon
-        {
-            Flag, Question, None
-        }
-
-        public Icon Icon{ get; set; }
+        public IconType Icon { get; set; }
         public int Id { get; set; }
         public double LocalOutlierFactor { get; set; }
         public string FormattetLocalOutlierFactor { get {return LocalOutlierFactor.ToString("2:N5");} }
